3e7f2121c597d6db3a79c06592e9e4cf23c72f1e
yesuc <ycarter@colgate.edu>
2018-12-28 21:06:02 -0500

    added home and contact pages

--- myCtrl.js
+++ myCtrl.js
@@ -1,4 +1,5 @@
 app.controller('myCtrl',function($scope) {
-  $scope.gpa=3.5;
+  $scope.gpa=3.55;
   $scope.name="John";
+  $scope.pLangList=[{name:'Java', prof:'high'}, {name:'Python', prof:'High'}, {name:'Ruby', prof:'High'}];
 });
