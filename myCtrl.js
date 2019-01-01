f8cecf80e41b3fbca3cbef35c5cd6ac6dc5d6de9
yesuc <ycarter@colgate.edu>
2019-01-01 12:11:43 -0500

    More progress in learning AngJS

--- myCtrl.js
+++ myCtrl.js
@@ -2,4 +2,12 @@ app.controller('myCtrl',function($scope) {
   $scope.gpa=3.55;
   $scope.name="John";
   $scope.pLangList=[{name:'Java', prof:'high'}, {name:'Python', prof:'High'}, {name:'Ruby', prof:'High'}];
+  $scope.myName="Click Me!"
+  $scope.changeName= function() {
+    $scope.myName="Yesu Carter"
+  };
+  $scope.functionOut= function() {
+    return $scope.name + " was born in " + $scope.year + " (I came from a function!)";
+  };
+
 });
