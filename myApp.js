2c021884be5dca0e2a717abf54799c504149162e
yesuc <ycarter@colgate.edu>
2019-01-02 14:26:08 -0500

    Completed W3 School

--- myApp.js
+++ myApp.js
@@ -1,1 +1,13 @@
 var app = angular.module('myApp',["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/home", {
+        templateUrl : "./home.html"
+    })
+    .when("/resume", {
+        templateUrl : "./resume.html"
+    })
+    .when("/contact", {
+        templateUrl : "./contact.html"
+    });
+});
