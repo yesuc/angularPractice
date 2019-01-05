51e0f88300505482c02d6a11bb133b0be2489896
yesuc <ycarter@colgate.edu>
2019-01-04 20:41:17 -0500

    Filling in content on home page

--- myCtrl.js
+++ myCtrl.js
@@ -9,5 +9,6 @@ app.controller('myCtrl',function($scope) {
   $scope.functionOut= function() {
     return $scope.name + " was born in " + $scope.year + " (I came from a function!)";
   };
-
+  $scope.current_projects=[{name:'Personal Website', desc:'Building a personal website using the newly learned AngularJS Framework, HTML, JavaScript, and CSS. Current objective: increase job application responses (even rejections!).'}];
+  $scope.recent_projects= [{}];
 });
